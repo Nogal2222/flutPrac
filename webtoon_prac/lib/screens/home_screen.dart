deeec78842e7fffa5ebf4dd8680bcc4769739dcc
njh3321 <njh3321@naver.com>
2023-04-28 17:53:24 +0900

    feat : webtoon 연습중 (listview, navigate, hero)

--- webtoon_prac/lib/screens/home_screen.dart
+++ webtoon_prac/lib/screens/home_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_prac/models/webtoon_model.dart';
+import 'package:webtoon_prac/servieces/api_service.dart';
+import 'package:webtoon_prac/widgets/webtoon_widget.dart';
 
+// stateful을 이용한 기초적인 방식의 Data 사용
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   List<WebtoonModel> webtoons = [];
+//   bool isLoading = true;
+
+//   void waitForWebToons() async {
+//     webtoons = await ApiService.getTodaysToons();
+//     isLoading = false;
+//     setState(() {});
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     waitForWebToons();
+//   }
+
+// FutureBuilder를 이용해서 Stateless로 간단히 구현할 수 있음.
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -19,6 +50,74 @@ class HomeScreen extends StatelessWidget {
             fontWeight: FontWeight.w400,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          // 데이터가 있을 때 UI
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  // child: makeList_builder(snapshot),
+                  child: makeListSeperated(snapshot),
+                ),
+              ],
+            );
+          }
+          // 에러가 발생했을 때 UI
+          else if (snapshot.hasError) {
+            return const Text("Error Occured");
+          }
+          // 데이터가 들어오기 전 UI
+          return Container(
+            color: Colors.black.withOpacity(0.3),
+            child: const Center(
+              child: CircularProgressIndicator(
+                color: Colors.green,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  // 보이는 화면만 Build 할 수 있도록 하는 ListView (builder)
+  ListView makeListBuilder(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Text(webtoon.title);
+      },
+    );
+  }
+
+  // 리스트 사이에 widget을 넣을 수 있는 ListView (seperated)
+  ListView makeListSeperated(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        // 커스텀 컴포넌트 사용방법
+        return WebtoonWidget(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
--- webtoon_prac/lib/servieces/api_service.dart
+++ webtoon_prac/lib/servieces/api_service.dart
@@ -1,19 +1,58 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:webtoon_prac/models/webtoon_detail_model.dart';
+import 'package:webtoon_prac/models/webtoon_episode_model.dart';
+import 'package:webtoon_prac/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
-  final String today = "today";
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = "today";
 
-  void getTodaysToons() async {
+  // async-await는 response가 오는 것을 기다려야 하기 떄문에 Future를 써야함
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+
     // pub.dev에서 패키지 찾을 수 있음 (http 패키지 설치)
     final url = Uri.parse('$baseUrl/$today');
     // 기본적으로 비동기식으로 작동
-    // http.get(url);
     // 동기식으로 작동하려면 다음과 같이 async + await
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        // 47개의 객체 파일
+        final webtoonInstance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(webtoonInstance);
+      }
+      return webtoonInstances;
     }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
+    }
+    throw Error();
   }
 }
